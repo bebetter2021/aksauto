0d23149aafc124d6e677692c3473cbdd16bbc389
scynte <stephen.golovich@sbcglobal.net>
2022-02-27 09:11:25 -0600

    updated config for github workflow

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "remote" {
-    organization = "Diehlabs"
+    organization = "Bebetter"
     workspaces {
-      name = "k8sauto-aks-tgo"
+      name = "aksauto"
     }
   }
 }
